07974a57d377637fbc7be7d2c54a55c20301d20c
Janet Mugambi <janet.mugambi@equitybank.co.ke>
2024-10-24 16:05:04 +0300

    Implement references section

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
+  references:any;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 
 }
+
