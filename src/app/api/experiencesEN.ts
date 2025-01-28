117d1f680662642f2da5ca669d51a13463526056
Janet Mugambi <janet.mugambi@equitybank.co.ke>
2025-01-28 15:59:11 +0300

    Implement CV section

---
+++ src/app/api/experiencesEN.ts
@@ -0,0 +1,46 @@
+
+export const experiencesEn = [
+    {
+        title: "Front-end developer",
+        company: 'Equity Bank LTD',
+        period: "2023 - Present",
+        technologies: 'Javascript, Typescript, Angular , Angular Material, HTML, CSS',
+        missions: [
+            "Analyzed and swiftly addressed development roadblocks, devising innovative solutions to ensure smooth and timely project progression.",
+            "Spearheaded creative solutions tailored to banking-specific challenges.",
+            "Collaborated closely with the business and operations teams, engaging with stakeholders and branch representatives to understand and address critical banking needs.",
+            "Transformed Figma design prototypes into functional and responsive web interfaces, ensuring seamless user experiences aligned with the design team's vision",
+            "Wrote and executed comprehensive tests for code functionality, accuracy, and reliability. Utilized automated and manual testing frameworks to guarantee smooth, error-free deployments and robust system stability.",
+            "Played a key role in migrating core systems from Version 1 to Version 2, incorporating new angular libraries and features.",
+            "Trained and guided new team members on coding standards, best practices, and banking-specific application development",
+            "Documented and maintained comprehensive process flows and system workflows to ensure clarity and consistency across system operations"
+
+        ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Software Engineering Intern",
+        company: 'Equity Bank LTD',
+        period: "2021 (July-September)",
+        technologies: 'Python,HTML,CSS,JavaScript',
+        missions: [
+            "Strategized and led extensive research on Face Recognition capabilities and Microsoft Azure cognitive services.",
+            "Designed and developed prototype for Face recognition for digital identity.",
+            "Collaborated with the Human resource department in the development of a new Human Resource Management System",
+            "Designed and developed Human Resource Management System"
+        ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Software Development Intern",
+        company: 'Intelligent Technologies',
+        period: "2018 (May-September)",
+        technologies: 'JAVA,XML,Android Studio,Firebase,Linux,Laravel,PHP,Javascript',
+        missions: [
+            "Collaborated with peers to plan and successfully execute various cross-platform applications",
+            "Designed and developed Web Application for managing Hostel placement in Universities",
+            
+        ],
+        detailIsDisplayed: false,
+    },
+];
--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as awesom from '@fortawesome/free-solid-svg-icons';
-
+import { experiencesEn } from '../api/experiencesEN';
 
 @Component({
   selector: 'app-cv',
@@ -10,17 +10,20 @@ import * as awesom from '@fortawesome/free-solid-svg-icons';
 export class CvComponent implements OnInit{
   experiences:any;
   planeIcon: any ;
-  gameIcon:any;
-  volleyBallIcon:any;
+  cookingIcon:any;
+  swimmingIcon:any;
+  documentaryIcon:any;
 
   constructor() {
-
+        this.experiences=experiencesEn;
   }
 
   ngOnInit(): void {
     this.planeIcon=awesom.faPlane;
-    this.gameIcon=awesom.faGamepad;
-    this.volleyBallIcon=awesom.faVolleyballBall;
+    this.cookingIcon=awesom.faKitchenSet;
+    this.swimmingIcon=awesom.faSwimmer;
+    this.documentaryIcon=awesom.faTelevision;
+
   }
   
   detailOnClick(experience:any) {
